203809f2e519baf5b4e15bd5e53febe7fc36ec80
Brian Wong <brianw7218@gmail.com>
2023-02-02 21:15:21 -0800

    Add an education section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import HeaderSection from "./components/HeaderSection";
+import EducationSection from "./components/EducationSection";
 
 function App() {
-  return <HeaderSection />;
+  return (
+    <div>
+      <HeaderSection />
+      <EducationSection />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/EducationSection.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class EducationSection extends React.Component {
+  render() {
+    return (
+      <>
+        <form className="education"></form>
+      </>
+    );
+  }
+}
+
+export default EducationSection;

d7dc9c5591ee330ef5db0c56e1a2f6c15cfc2c26
Brian Wong <brianw7218@gmail.com>
2023-02-04 18:00:28 -0800

    Add import statement

--- src/components/WorkHistorySection.js
+++ src/components/WorkHistorySection.js
@@ -1,5 +1,6 @@
 import React from "react";
 import AddItemButton from "./AddItemButton";
+import WorkHistoryElement from "./WorkHistoryElement";
 
 class WorkHistorySection extends React.Component {
   constructor(props) {

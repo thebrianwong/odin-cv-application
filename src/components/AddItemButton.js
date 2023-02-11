90fc2c0fa534e0fa0add55dfe48efdc6f7f59d1b
Brian Wong <brianw7218@gmail.com>
2023-02-10 21:05:42 -0800

    Remove unnecessary constructor

--- src/components/AddItemButton.js
+++ src/components/AddItemButton.js
@@ -1,9 +1,6 @@
 import React from "react";
 
 class AddItemButton extends React.Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     const { addItem } = this.props;
     return (

8494f8cd9936d32432f317db9041a4ae999e8dcf
Brian Wong <brianw7218@gmail.com>
2023-02-10 21:09:39 -0800

    Add css normalizer and stylesheet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import HeaderSection from "./components/HeaderSection";
 import EducationSection from "./components/EducationSection";
 import WorkHistorySection from "./components/WorkHistorySection";
+import "./styles/normalize.css";
+import "./styles/styles.css";
 
 function App() {
   return (

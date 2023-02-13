01cd907e8800f56fdaef18358dc5ed997ec89dad
Brian Wong <brianw7218@gmail.com>
2023-02-12 22:31:41 -0800

    Convert header element component to functional component

--- src/components/HeaderElement.js
+++ src/components/HeaderElement.js
@@ -1,77 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import CancelEditButton from "./CancelEditButton";
 import EditSubmitButtons from "./EditSubmitButtons";
 import Input from "./Input";
 
-class HeaderElement extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: props.value, editing: false };
-  }
-  render() {
-    const { className, headerType, inputType, updateHeader } = this.props;
-    let placeholder;
+const HeaderElement = ({
+  className,
+  startingValue,
+  headerType,
+  inputType,
+  updateHeader,
+}) => {
+  const [value, setValue] = useState(startingValue);
+  const [editing, setEditing] = useState(false);
+
+  const determinePlaceholder = () => {
     if (headerType === "name") {
-      placeholder = "Name";
+      return "Name";
     } else if (headerType === "email") {
-      placeholder = "Email";
+      return "Email";
     } else if (headerType === "phone") {
-      placeholder = "Phone Number";
+      return "Phone Number";
     }
-    const handleChange = (newValue) => {
-      this.setState({ ...this.state, value: newValue });
-    };
-    const startEdit = () => {
-      this.setState({ editing: true });
-    };
-    const submitEdit = () => {
-      this.setState({ editing: false });
-      updateHeader(this.state.value, headerType);
-    };
-    const resetState = () => {
-      this.setState({ value: this.props.value });
-    };
-    const cancelEdit = () => {
-      this.setState({ editing: false });
-      resetState();
-    };
-    return (
-      <div className="header-item">
-        {!this.state.editing ? (
-          <p className={`header-text ${className}`}>
-            {this.props.value.length === 0 ? placeholder : this.props.value}
-          </p>
-        ) : (
-          <Input
-            focus={true}
-            label={placeholder}
-            className="header-input"
-            previousValue={this.state.value}
-            inputType={inputType}
-            informationType={headerType}
-            sendChanges={handleChange}
-            submitEdit={submitEdit}
-            cancelEdit={cancelEdit}
-          />
-        )}
-        <EditSubmitButtons
-          generalClassName="header-button"
-          editClassName="header-edit-button"
-          submitClassName="header-submit-button"
-          editing={this.state.editing}
-          startEdit={startEdit}
+  };
+  const placeholder = determinePlaceholder();
+
+  const handleChange = (newValue) => {
+    setValue(newValue);
+  };
+  const startEdit = () => {
+    setEditing(true);
+  };
+  const submitEdit = () => {
+    setEditing(false);
+    updateHeader(value, headerType);
+  };
+  const resetState = () => {
+    setValue(startingValue);
+  };
+  const cancelEdit = () => {
+    setEditing(false);
+    resetState();
+  };
+
+  return (
+    <div className="header-item">
+      {!editing ? (
+        <p className={`header-text ${className}`}>
+          {value.length === 0 ? placeholder : value}
+        </p>
+      ) : (
+        <Input
+          focus={true}
+          label={placeholder}
+          className="header-input"
+          previousValue={value}
+          inputType={inputType}
+          informationType={headerType}
+          sendChanges={handleChange}
           submitEdit={submitEdit}
+          cancelEdit={cancelEdit}
         />
-        {this.state.editing && (
-          <CancelEditButton
-            generalClassName="header-button"
-            specificClassName="header-cancel-edit-button"
-            cancelEdit={cancelEdit}
-          />
-        )}
-      </div>
-    );
-  }
-}
+      )}
+      <EditSubmitButtons
+        generalClassName="header-button"
+        editClassName="header-edit-button"
+        submitClassName="header-submit-button"
+        editing={editing}
+        startEdit={startEdit}
+        submitEdit={submitEdit}
+      />
+      {editing && (
+        <CancelEditButton
+          generalClassName="header-button"
+          specificClassName="header-cancel-edit-button"
+          cancelEdit={cancelEdit}
+        />
+      )}
+    </div>
+  );
+};
 
 export default HeaderElement;

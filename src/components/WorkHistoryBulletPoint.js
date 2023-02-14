807a5d65e34a72201f9b16589cb75a212e484e39
Brian Wong <brianw7218@gmail.com>
2023-02-13 21:30:03 -0800

    Convert work history bullet point component into a functional component

--- src/components/WorkHistoryBulletPoint.js
+++ src/components/WorkHistoryBulletPoint.js
@@ -1,79 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import CancelEditButton from "./CancelEditButton";
 import DeleteItemButton from "./DeleteItemButton";
 import EditSubmitButtons from "./EditSubmitButtons";
 import Input from "./Input";
 
-class WorkHistoryBulletPoint extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: "", editing: true };
-  }
-  render() {
-    const { itemIndex, taskIndex, description, updateTask, deleteTask } =
-      this.props;
-    const handleChanges = (newValue) => {
-      this.setState({ value: newValue });
-    };
-    const startEdit = () => {
-      this.setState({ editing: true });
-    };
-    const submitEdit = () => {
-      this.setState({ editing: false });
-      updateTask(this.state.value, itemIndex, taskIndex);
-    };
-    const resetState = () => {
-      this.setState({ value: this.props.description });
-    };
-    const cancelEdit = () => {
-      this.setState({ editing: false });
-      resetState();
-    };
-    return (
-      <li className="task-bullet-point">
-        <div>
-          {!this.state.editing ? (
-            <p className="task-bullet-point-description">{description}</p>
-          ) : (
-            <Input
-              focus={true}
-              previousValue={this.state.value}
-              inputType="text"
-              sendChanges={handleChanges}
-              submitEdit={submitEdit}
+const WorkHistoryBulletPoint = ({
+  itemIndex,
+  taskIndex,
+  description,
+  updateTask,
+  deleteTask,
+}) => {
+  const [value, setValue] = useState("");
+  const [editing, setEditing] = useState(true);
+
+  const handleChanges = (newValue) => {
+    setValue(newValue);
+  };
+  const startEdit = () => {
+    setEditing(true);
+  };
+  const submitEdit = () => {
+    setEditing(false);
+    updateTask(value, itemIndex, taskIndex);
+  };
+  const resetState = () => {
+    setValue(description);
+  };
+  const cancelEdit = () => {
+    setEditing(false);
+    resetState();
+  };
+  return (
+    <li className="task-bullet-point">
+      <div>
+        {!editing ? (
+          <p className="task-bullet-point-description">{description}</p>
+        ) : (
+          <Input
+            focus={true}
+            previousValue={value}
+            inputType="text"
+            sendChanges={handleChanges}
+            submitEdit={submitEdit}
+            cancelEdit={cancelEdit}
+          />
+        )}
+        <div className="button-group">
+          <EditSubmitButtons
+            generalClassName="work-history-task-button"
+            editClassName="work-history-task-edit-button"
+            submitClassName="work-history-task-submit-button"
+            editing={editing}
+            startEdit={startEdit}
+            submitEdit={submitEdit}
+          />
+          {editing && (
+            <CancelEditButton
+              generalClassName="work-history-task-button"
+              specificClassName="work-history-task-cancel-edit-button"
               cancelEdit={cancelEdit}
             />
           )}
-          <div className="button-group">
-            <EditSubmitButtons
+          {!editing && (
+            <DeleteItemButton
               generalClassName="work-history-task-button"
-              editClassName="work-history-task-edit-button"
-              submitClassName="work-history-task-submit-button"
-              editing={this.state.editing}
-              startEdit={startEdit}
-              submitEdit={submitEdit}
+              specificClassName="work-history-task-delete-button"
+              itemIndex={itemIndex}
+              taskIndex={taskIndex}
+              deleteFromCollection={deleteTask}
             />
-            {this.state.editing && (
-              <CancelEditButton
-                generalClassName="work-history-task-button"
-                specificClassName="work-history-task-cancel-edit-button"
-                cancelEdit={cancelEdit}
-              />
-            )}
-            {!this.state.editing && (
-              <DeleteItemButton
-                generalClassName="work-history-task-button"
-                specificClassName="work-history-task-delete-button"
-                itemIndex={itemIndex}
-                taskIndex={taskIndex}
-                deleteFromCollection={deleteTask}
-              />
-            )}
-          </div>
+          )}
         </div>
-      </li>
-    );
-  }
-}
+      </div>
+    </li>
+  );
+};
 
 export default WorkHistoryBulletPoint;

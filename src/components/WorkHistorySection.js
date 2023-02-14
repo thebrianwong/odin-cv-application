9557065c6429ec11dc2f40eee6b4c7873ef59b18
Brian Wong <brianw7218@gmail.com>
2023-02-13 21:59:09 -0800

    Convert work history section into a functional component

--- src/components/WorkHistorySection.js
+++ src/components/WorkHistorySection.js
@@ -1,139 +1,132 @@
-import React from "react";
+import React, { useState } from "react";
 import AddItemButton from "./AddItemButton";
 import WorkHistoryElement from "./WorkHistoryElement";
 
-class WorkHistorySection extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { items: [], IDCounter: 0 };
-  }
-  render() {
-    const updateTask = (newDescription, itemIndex, taskIndex) => {
-      this.setState({
-        items: this.state.items.map((item) => {
-          if (item.itemID === itemIndex) {
-            return {
-              ...item,
-              tasks: item.tasks.map((task) => {
-                if (task.taskID === taskIndex) {
-                  return {
-                    ...task,
-                    description: newDescription,
-                  };
-                } else {
-                  return task;
-                }
-              }),
-            };
-          } else {
-            return item;
-          }
-        }),
-      });
-    };
-    const addTask = (itemIndex) => {
-      this.setState({
-        items: this.state.items.map((item) => {
-          if (item.itemID === itemIndex) {
-            return {
-              ...item,
-              tasks: item.tasks.concat({
-                description: "",
-                taskID: item.IDCounter,
-              }),
-              IDCounter: item.IDCounter + 1,
-            };
-          } else {
-            return item;
-          }
-        }),
-      });
-    };
-    const deleteTask = (itemIndex, taskIndex) => {
-      this.setState({
-        items: this.state.items.map((item) => {
-          if (item.itemID === itemIndex) {
-            return {
-              ...item,
-              tasks: item.tasks.filter((task) => task.taskID !== taskIndex),
-            };
-          } else {
-            return item;
-          }
-        }),
-      });
-    };
-    const updateItem = (
-      newCompany,
-      newTitle,
-      newStartDate,
-      newEndDate,
-      itemIndex
-    ) => {
-      this.setState({
-        items: this.state.items.map((item) => {
-          if (item.itemID === itemIndex) {
-            return {
-              ...item,
-              companyName: newCompany,
-              title: newTitle,
-              employmentDates: { start: newStartDate, end: newEndDate },
-            };
-          } else {
-            return item;
-          }
-        }),
-      });
-    };
-    const addItem = () => {
-      this.setState({
-        items: this.state.items.concat({
-          companyName: "",
-          title: "",
-          tasks: [],
-          employmentDates: { start: "", end: "" },
-          itemID: this.state.IDCounter,
-          IDCounter: 0,
-        }),
-        IDCounter: this.state.IDCounter + 1,
-      });
-    };
-    const deleteItem = (itemIndex) => {
-      this.setState({
-        ...this.state,
-        items: this.state.items.filter((item) => item.itemID !== itemIndex),
-      });
-    };
-    return (
-      <div className="work-history-section">
-        <div className="work-history-header">
-          <h2>Work History</h2>
-          <AddItemButton
-            generalClassName="add-item-button"
-            specificClassName="add-work-history-item-button"
-            addItem={addItem}
-          />
-        </div>
-        {this.state.items.length === 0 && <p>Add your work history here!</p>}
-        {this.state.items.map((item) => (
-          <WorkHistoryElement
-            itemIndex={item.itemID}
-            key={item.itemID}
-            companyName={item.companyName}
-            title={item.title}
-            tasks={item.tasks}
-            startDate={item.employmentDates.start}
-            endDate={item.employmentDates.end}
-            updateItem={updateItem}
-            addTask={addTask}
-            updateTask={updateTask}
-            deleteItem={deleteItem}
-            deleteTask={deleteTask}
-          />
-        ))}
-      </div>
+const WorkHistorySection = () => {
+  const [items, setItems] = useState([]);
+  const [IDCounter, setIDCounter] = useState(0);
+  const updateTask = (newDescription, itemIndex, taskIndex) => {
+    setItems(
+      items.map((item) => {
+        if (item.itemID === itemIndex) {
+          return {
+            ...item,
+            tasks: item.tasks.map((task) => {
+              if (task.taskID === taskIndex) {
+                return {
+                  ...task,
+                  description: newDescription,
+                };
+              } else {
+                return task;
+              }
+            }),
+          };
+        } else {
+          return item;
+        }
+      })
     );
-  }
-}
+  };
+  const addTask = (itemIndex) => {
+    setItems(
+      items.map((item) => {
+        if (item.itemID === itemIndex) {
+          return {
+            ...item,
+            tasks: item.tasks.concat({
+              description: "",
+              taskID: item.IDCounter,
+            }),
+            IDCounter: item.IDCounter + 1,
+          };
+        } else {
+          return item;
+        }
+      })
+    );
+  };
+  const deleteTask = (itemIndex, taskIndex) => {
+    setItems(
+      items.map((item) => {
+        if (item.itemID === itemIndex) {
+          return {
+            ...item,
+            tasks: item.tasks.filter((task) => task.taskID !== taskIndex),
+          };
+        } else {
+          return item;
+        }
+      })
+    );
+  };
+  const updateItem = (
+    newCompany,
+    newTitle,
+    newStartDate,
+    newEndDate,
+    itemIndex
+  ) => {
+    setItems(
+      items.map((item) => {
+        if (item.itemID === itemIndex) {
+          return {
+            ...item,
+            companyName: newCompany,
+            title: newTitle,
+            employmentDates: { start: newStartDate, end: newEndDate },
+          };
+        } else {
+          return item;
+        }
+      })
+    );
+  };
+  const addItem = () => {
+    setItems(
+      items.concat({
+        companyName: "",
+        title: "",
+        tasks: [],
+        employmentDates: { start: "", end: "" },
+        itemID: IDCounter,
+        IDCounter: 0,
+      })
+    );
+    setIDCounter(IDCounter + 1);
+  };
+  const deleteItem = (itemIndex) => {
+    setItems(items.filter((item) => item.itemID !== itemIndex));
+  };
+  return (
+    <div className="work-history-section">
+      <div className="work-history-header">
+        <h2>Work History</h2>
+        <AddItemButton
+          generalClassName="add-item-button"
+          specificClassName="add-work-history-item-button"
+          addItem={addItem}
+        />
+      </div>
+      {items.length === 0 && <p>Add your work history here!</p>}
+      {items.map((item) => (
+        <WorkHistoryElement
+          itemIndex={item.itemID}
+          key={item.itemID}
+          companyName={item.companyName}
+          title={item.title}
+          tasks={item.tasks}
+          startDate={item.employmentDates.start}
+          endDate={item.employmentDates.end}
+          updateItem={updateItem}
+          addTask={addTask}
+          updateTask={updateTask}
+          deleteItem={deleteItem}
+          deleteTask={deleteTask}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default WorkHistorySection;
